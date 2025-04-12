71c63b0f0b3f3108fb87c1dd6e87749b6418ca52
ArjunM2005 <135345989+ArjunM2005@users.noreply.github.com>
2025-04-12 22:15:34 +0530

    Added Week1 with longestconssq.py

---
+++ Week1/Day2/longestconssq.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+      s = set(nums)
+      longest = 0
+      for num in s:
+        if num-1 not in s:
+            curr = 1
+            while num+1 in s:
+                curr+=1
+                num+=1
+            longest = max(longest,curr)
+        return longest
+        
